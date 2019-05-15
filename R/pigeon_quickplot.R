3aac4cbc3aecc0c94c9d12d7fb7cc77ce001ad65
NourAl-Zaghloul <nzaghlou@andrew.cmu.edu>
2019-05-15 15:18:25 -0400

    quickplot add

---
+++ R/pigeon_quickplot.R
@@ -0,0 +1,22 @@
+#' Quick ggplot2 parameters
+#'
+#' @param method What settings you want
+#' @example
+#' pigeon_plot("boxjitter")
+pigeon_quickplot <- function(method = "default"){
+  ##$ Made for quick exploration, not actual plotting
+
+  ##$ Simple Box & Jitter plot
+  if (method == "default" | method == "boxjitter"){
+
+    list(
+      ggplot2::geom_bar(stat = "summary", position = "dodge",fun.y = "mean", width =.5, alpha = .6),
+      ggplot2::geom_jitter(width = .3),
+      ggplot2::geom_hline(aes(yintercept = 0.0), color = "black", linetype = "dashed"),
+      ggplot2::stat_summary(geom = "errorbar", fun.data = mean_se, position = "dodge", width = .1)
+    )
+
+  }
+
+}
+
