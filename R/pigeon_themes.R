fdbd9ead61fac3869e5a857f0f61cf49bb27b63e
Nour al-Zaghloul <Nouralzaghloul@gmail.com>
2020-05-06 16:41:20 -0400

    themer

--- R/pigeon_themes.R
+++ R/pigeon_themes.R
@@ -6,10 +6,14 @@
 library(tidyverse)
 library(patchwork)
 
-rihanna <- readRDS("Rihanna_spotify.RDS")
-rihanna <- rihanna %>%
+rihanna_data <- readRDS("Rihanna_spotify.RDS")
+rihanna <- rihanna_data %>%
   filter(!grepl("\\(",album_name),
          !grepl(":",album_name))
+rihanna_rereleases <- rihanna_data %>%
+  filter(grepl("[\\(:]",album_name))
+
+
 
 plt_scatter <- ggplot(rihanna, aes(x = duration_ms, y = tempo, color = album_name)) +
   geom_jitter()
@@ -22,6 +26,31 @@ rihanna_grouped <- rihanna %>%
   group_by(album_name) %>%
   summarise(loud_mean = mean(loudness),
             year = mean(album_release_year))
+
+rihanna_grouped_keyYear <- rihanna_data %>%
+  group_by(key_name, album_release_year, mode) %>%
+  summarise(avgDance = mean(danceability),
+            avgEnergy = mean(energy),
+            avgSpeech = mean(speechiness))
+
+facet_jitter <- ggplot(rihanna_grouped_keyYear, aes(x = album_release_year, y = avgSpeech, color = key_name)) +
+  geom_line(position = position_jitter(width = .025, height = .025, seed = 112211)) +
+  geom_point(position = position_jitter(width = .025, height = .025, seed = 112211)) +
+  facet_wrap(vars(mode), nrow = 2) +
+  theme_minimal()
+
+nofacet_jitter <- ggplot(rihanna_grouped_keyYear, aes(x = album_release_year, y = avgSpeech, color = key_name)) +
+  geom_line(position = position_jitter(width = .025, height = .025, seed = 112211)) +
+  geom_point(position = position_jitter(width = .025, height = .025, seed = 112211)) +
+  theme_minimal()
+
+facet_jitter / nofacet_jitter
+
+# ggplot(rihanna_data, aes(x = album_release_year, y = speechiness, color = key_name)) +
+#   facet_wrap(vars(key_mode)) +
+#   geom_line(position = position_jitter(width = .25, height = .25, seed = 112211)) +
+#   geom_point(position = position_jitter(width = .25, height = .25, seed = 112211))
+
 
 plt_line <- ggplot(rihanna_grouped, aes(x = year, y = loud_mean)) +
   geom_line() +
